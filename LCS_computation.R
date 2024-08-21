11ecb2719aea3789b98b83e81b5c01a94ee97ac7
seb <96783701+zetcheuv@users.noreply.github.com>
2024-08-21 17:48:52 +0200

    Update LCS_computation.R

--- LCS_computation.R
+++ LCS_computation.R
@@ -28,7 +28,7 @@ TRANS_scoring <- TRANS_scoring[names(scoringSig),]
 TRANS_scoring <- t(scale(t(TRANS_scoring)))
 # 2) remove the last permille (upper outlier effect dampening, the lowest outliers have no other effect than diminishing the scores)
 # OPTIONAL - may be useful for big signatures, a bit less for small ones
-#TRANS_scoring[TRANS_scoring > quantile(TRANS_scoring,0.999)] <- quantile(TRANS_scoring,0.999)
+TRANS_scoring[TRANS_scoring > quantile(TRANS_scoring,0.999)] <- quantile(TRANS_scoring,0.999)
 # 3) apply LCS over samples. LCS is dependant on the dataset (constitution, size, quality, ...)
 scores <- apply(TRANS_scoring, 2, function(x) mean(x * scoringSig) )
 # 4) compute Zscores from the scores, these will be more comparable across datasets
